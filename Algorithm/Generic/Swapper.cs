19dddbbc1b91fac0d83d6698ebc36558069544cc
ubacs <gaborubacs@gmail.com>
2020-01-01 21:07:01 +0100

    Swapper
    Add Swapper class and respective tests.

---
+++ Algorithm/Generic/Swapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ubacs.Algorithm.Generic
+{
+    public static class Swapper
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        public static void SwapElements<T>(IList<T> list, int i, int j)
+        {
+
+            if (list == null)
+            {
+                throw new ArgumentNullException(nameof(list), "The parameter can not be null.");
+            }
+            if (i < 0 || i >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(i), "Index was out of range. Must be non-negative and less than the size of the collection.");
+            }
+            if (j < 0 || j >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(j), "Index was out of range. Must be non-negative and less than the size of the collection.");
+            }
+
+            T tmp = list[i];
+            list[i] = list[j];
+            list[j] = tmp;
+        }
+    }
+}
---
+++ AlgorithmTests/Generic/SwapperTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+using ubacs.Algorithm.Generic;
+
+namespace ubacs.AlgorithmTests.Generic
+{
+    public class SwapperTests
+    {
+        //The following test will throw an exception because of a bug introduced (?) in XUnit 2.4:
+        //https://github.com/xunit/xunit/issues/1781
+        //Use the nongeneric test methods instead!
+
+        //[Theory]
+        //[InlineData(-1, 2)]
+        //[InlineData("abab", "cdcd")]
+        //[InlineData("abab", null)]
+        public void Swap_Correct<T>(T a, T b)
+        {
+            T expectedA = b;
+            T expectedB = a;
+            Swapper.Swap(ref a, ref b);
+
+            Assert.Equal(expectedA, a);
+            Assert.Equal(expectedB, b);
+        }
+
+        [Theory]
+        [InlineData(-1, 2)]
+        public void Swap_Int_Correct(int a, int b)
+        {
+            int expectedA = b;
+            int expectedB = a;
+            Swapper.Swap(ref a, ref b);
+
+            Assert.Equal(expectedA, a);
+            Assert.Equal(expectedB, b);
+        }
+
+        [Theory]
+        [InlineData("abab", "cdcd")]
+        [InlineData("abab", null)]
+        public void Swap_String_Correct(string a, string b)
+        {
+            string expectedA = b;
+            string expectedB = a;
+            Swapper.Swap(ref a, ref b);
+
+            Assert.Equal(expectedA, a);
+            Assert.Equal(expectedB, b);
+        }
+
+        [Fact]
+        public void SwapElements_ThrowsOnNullList()
+        {
+            List<int> list = null;
+            int i = 0;
+            int j = 0;
+            Assert.Throws<ArgumentNullException>(() => Swapper.SwapElements(list, i, j));
+        }
+
+        [Theory]
+        [InlineData(1, -1, 0)]
+        [InlineData(1, 0, -1)]
+        [InlineData(0, 0, 0)]
+        [InlineData(3, 3, 0)]
+        [InlineData(3, 0, 12)]
+        public void SwapElements_ThrowsOnIndexOutOfRange(int size, int i, int j)
+        {
+            List<int> list = Enumerable.Range(0, size).ToList();
+            Assert.Throws<ArgumentOutOfRangeException>(() => Swapper.SwapElements(list, i, j));
+        }
+
+        [Fact]
+        public void SwapElements_Correct()
+        {
+
+            List<int> list = Enumerable.Range(1, 10).ToList();
+            List<int> expected = list;
+            expected.Reverse();
+
+            int i = 0;
+            int j = list.Count - 1;
+            while (i < j)
+            {
+                Swapper.SwapElements(list, i, j);
+                i++;
+                j--;
+            }
+
+            Assert.Equal(expected, list);
+        }
+    }
+}
+
